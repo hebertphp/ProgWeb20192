74fd28124e104482999a77a8ba99279624b14d76
Hebertphp <hebertphp@gmail.com>
2019-10-16 08:04:25 -0300

    Create array2.php

---
+++ array/array2.php
@@ -0,0 +1,27 @@
+<?php
+
+$aluno["nome"]="Roberta";
+$aluno["ra"]=123456;
+$aluno["nota"]=7.5;
+
+
+$aluno["nota"]=$aluno["nota"]+0.5;
+$aluno["nome"]=$aluno["nome"]." da Silva";
+
+
+
+
+echo "Nota = ".$aluno["nota"];// Nota = 7.5 
+
+echo "<pre>"; print_r($_SERVER); echo "</pre>";
+
+$produto=array( "nome" => "Notebook",
+               "preco" => 3500.50,
+               "quantidade" => 15);
+
+echo "Quantidade = "._________;// Quantidade = 15 7.5			   
+echo "<pre>"; print_r($aluno); echo "</pre>";
+
+
+
+?>
