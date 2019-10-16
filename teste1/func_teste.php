6b086377207fc7c1444c1d5ebd6d8ec7af0094ca
Hebertphp <hebertphp@gmail.com>
2019-10-16 19:10:24 -0300

    teste1

---
+++ teste1/func_teste.php
@@ -0,0 +1,18 @@
+<?php
+//exemplo de criação de funções
+function mostra($msg){
+  echo "<b>$msg</b>";
+}
+function calc($n1,$n2,$op){
+    if($op=="+"){
+       $res=$n1+$n2;
+    }
+  return $res;
+}  
+
+function parimpar($n){  
+  $msg=($n%2==0)?"PAR":"ÍMPAR";
+  return $msg;
+}
+
+?>
