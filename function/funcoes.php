4b915d9c3a5f682b3b7ac480606f3344f95d3802
Hebertphp <hebertphp@gmail.com>
2019-10-16 10:07:06 -0300

    Update funcoes.php

--- function/funcoes.php
+++ function/funcoes.php
@@ -2,9 +2,27 @@
 function mostra($msg){
   echo "<b>$msg</b>";
 }
+
 function calc($n1,$n2,$op){
     if($op=="+"){
        $res=$n1+$n2;
+    }
+    elseif($op=="-"){
+        $res=$n1-$n2;
+    }
+    elseif($op=="*"){
+        $res=$n1*$n2;
+    }
+    elseif($op=="/"){
+        if($n2==0){
+            $res="Divisão por zero!";
+        }
+        else{
+            $res=$n1/$n2;
+        }
+    }
+    else{
+        $res="Operador desconhecido!";
     }
   return $res;
 }  
@@ -14,4 +32,26 @@ function parimpar($n){
   return $msg;
 }
 
+function dozero($zero, $um){
+    $resultado=$zero * $um;
+    return $resultado;
+}
+
+mostra("Vai que funciona!!!");
+echo "<br>";
+echo dozero(7,3);
+echo "<br>";
+echo parimpar(8);
+echo "<br>";
+echo calc(4,3,"+");
+echo "<br>";
+echo calc(4,3,"-");
+echo "<br>";
+echo calc(4,3,"*");
+echo "<br>";
+echo calc(4,3,"/");
+echo "<br>";
+echo calc(4,0,"/");
+echo "<br>";
+echo calc(4,3,"?");
 ?>
